ead6cabcbc2d2e59882669d54074a8ebe034d398
Danilo ribeiro <danilo.ribeiro@noverde.com.br>
2020-05-14 20:17:56 +0000

    remove console

--- use-cases/login.js
+++ use-cases/login.js
@@ -1,18 +1,14 @@
 const { jwtSign } = require('../infrastructure/jwt-validation');
 
 module.exports = (req, res) => {
-  console.log('LoginUseCase');
   const { user, pass } = req.body;
-  console.log('LoginUseCase 1');
 
   if (user !== 'user' || pass !== 'pass') {
     res.status(401).send({ status: 'Access Denied' });
     return;
   }
-  console.log('LoginUseCase 2');
 
   const token = jwtSign(req.body);
-  console.log('LoginUseCase 3');
 
   res.send({ status: 'Access Granted', Authorization: token });
 };
