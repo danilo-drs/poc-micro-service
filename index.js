fea33061f1e49cc90c2f456e073c57b5ece33591
Danilo ribeiro <danilo.ribeiro@noverde.com.br>
2020-05-14 19:36:30 +0000

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const LoginUseCase = require('./use-cases/login');
 const OrdersUseCase = require('./use-cases/orders');
 const DetailUseCase = require('./use-cases/detail');
@@ -7,7 +8,7 @@ const { jwtValidationMiddleware } = require('./infrastructure/jwt-validation');
 
 // infrastructure
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(jwtValidationMiddleware);
 
